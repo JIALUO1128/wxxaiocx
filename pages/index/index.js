3163884db97d04621531c4f235835ec429f2e1d7
JIALO <jyls1128@gmail.com>
2024-04-01 20:48:20 +0800

    首页中重写的内容

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,35 +1,41 @@
 // index.js
 Page({
-  icons:[
-    {
-      name:'icon1',
-      title:'学校概况',
-      imgUrl:'/img/images/icons/setting-icons/icon1.png',
-      backgroundColor:'F6B059'
-    },
-    {
-      name:'icon2',
-      title:'成绩查询',
-      imgUrl:'/img/images/icons/setting-icons/icon2.png',
-      backgroundColor:'F6B059'
-    },
-    {
-      name:'icon3',
-      title:'缴费入口',
-      imgUrl:'/img/images/icons/setting-icons/icon3.png',
-      backgroundColor:'EC1E71'
-    },
-    {
-      name:'icon4',
-      title:'学院预警',
-      imgUrl:'/img/images/icons/setting-icons/icon4.png',
-      backgroundColor:'03E391'
-    },
-    {
-      name:'icon5',
-      title:'全部',
-      imgUrl:'/img/images/icons/setting-icons/icon5.png',
-      backgroundColor:'031E9DFF'
-    },
-  ]
+  data:{
+    background: ['tabbar1', 'tabbar2', 'tabbar3'],
+    icons: [{
+      name: 'icon1',
+      title: '学校概况',
+      imgUrl: '/img/images/icons/icon1.png',
+      backgroundColor: '#F6B059'
+    }, {
+      name: 'icon2',
+      title: '成绩查询',
+      imgUrl: '/img/images/icons/icon2.png',
+      backgroundColor: '#8F6AFF'
+    }, {
+      name: 'icon3',
+      title: '缴费入口',
+      imgUrl: '/img/images/icons/icon3.png',
+      backgroundColor: '#EC1E71'
+    }, {
+      name: 'icon4',
+      title: '学院预警',
+      imgUrl: '/img/images/icons/icon4.png',
+      backgroundColor: '#03E391'
+    }, {
+      name: 'icon5',
+      title: '全部',
+      imgUrl: '/img/images/icons/icon5.png',
+      backgroundColor: '#031E9DFF'
+    }],
+     /* 推荐组件标题 */
+     texts: ['推荐应用','根据您最近浏览和办理应用为您推荐'],
+     indicatorDots: true,
+     vertical: false,
+     autoplay: true,
+     interval: 2000,
+     duration: 800,
+     circular: true
+  }
+  
 })

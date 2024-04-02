22df0c788ac9ef40060b7c9ae4f99ca790e06444
JIALO <jyls1128@gmail.com>
2024-04-02 21:22:22 +0800

    添加数据

--- pages/find/find.js
+++ pages/find/find.js
@@ -5,7 +5,70 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    // 通知卡片标题
+		informListTitle: [{
+			id: 1,
+			title: "通知公告",
+		}, {
+			id: 2,
+			title: "通知公告"
+		}, {
+			id: 3,
+			title: "综合信息"
+		}, {
+			id: 4,
+			title: "学术动态"
+		}],
+			// 通知卡片数据
+	informListCard: [{
+		currentTab: 0,
+		"text1": "关于加强新生入学后校园安全管理规范的通知",
+		"text2": "校园新闻网",
+		"imagePath": "../../img/images/card/card1.png",
+		"startBackgroundColor": "#b8fcfe",
+		"endBackgroundColor": "#defbfc",
+	},
+	{
+		currentTab: 0,
+		"text1": "关于开展2023新生入学教育系列培训课程的指导意见",
+		"text2": "信息化管理中心",
+		"imagePath": "../../img/images/card/card2.png",
+		"startBackgroundColor": "#8cb948",
+		"endBackgroundColor": "#cddcb7",
+	},
+	{
+		currentTab: 0,
+		"text1": "关于加强新生入学后校园安全管理条例实行的通知",
+		"text2": "教务处",
+		"imagePath": "../../img/images/card/card3.png",
+		"startBackgroundColor": "#b4fdff",
+		"endBackgroundColor": "#d1fafb",
+	},
+	{
+		currentTab: 1,
+		"text1": "关于加强新生入学后校园安全管理规范的通知",
+		"text2": "校园新闻网",
+		"imagePath": "../../img/images/card/card1.png",
+		"startBackgroundColor": "#b8fcfe",
+		"endBackgroundColor": "#defbfc",
+	},
+	{
+		currentTab: 2,
+		"text1": "关于开展2023新生入学教育系列培训课程的指导意见",
+		"text2": "信息化管理中心",
+		"imagePath": "../../img/images/card/card2.png",
+		"startBackgroundColor": "#8cb948",
+		"endBackgroundColor": "#cddcb7",
+	},
+	{
+		currentTab: 3,
+		"text1": "关于加强新生入学后校园安全管理条例实行的通知",
+		"text2": "教务处",
+		"imagePath": "../../img/images/card/card3.png",
+		"startBackgroundColor": "#b4fdff",
+		"endBackgroundColor": "#d1fafb",
+	},
+],
   },
 
   /**

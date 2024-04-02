bd13b3c74aee09afdd56918189903a27b9ffe9dc
JIALO <jyls1128@gmail.com>
2024-04-02 14:34:23 +0800

    添加head-model的js

--- components/head-model/head-model.js
+++ components/head-model/head-model.js
@@ -1,11 +1,38 @@
 // components/head-model/head-model.js
+const app = getApp()
 Page({
+  attached: function () {},
+  options: {
+    multipleSlots: true // 在组件定义时的选项中启用多slot支持
+  },
+  /* switchTab 跳转到message页面*/
+  switchTab() {
+    wx.switchTab({
+      url: '/pages/message/message'
+    })
+  },
+  //扫描二维码
+  scanMate() {
+    wx.scanCode({
+      onlyFromCamera: true, // 默认false，true为只允许从相机扫码
+      success: res => {
+        console.info(res); //打印出来，查看自己想要的返回结果数据
+        console.info(res.result); //打印二维码中的内容
+
+        // 下面写自己的业务逻辑
+
+      }
+    })
+  },
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    navBarHeight: app.globalData.navBarHeight,
+    menuRight: app.globalData.menuRight,
+    menuTop: app.globalData.menuTop,
+    menuHeight: app.globalData.menuHeight,
   },
 
   /**

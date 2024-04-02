ecea177b8e12f8b06a01ff735d5cd3ca42d8cfde
JIALO <jyls1128@gmail.com>
2024-04-02 14:34:23 +0800

    添加iconsjs

--- components/icons/icons.js
+++ components/icons/icons.js
@@ -1,66 +1,22 @@
-// components/icons/icons.js
-Page({
-
-  /**
-   * 页面的初始数据
-   */
+Component({
   data: {
 
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
+  properties: {
+    /* 接受icon数据 */
+    /*  {
+       id: 1,
+       title: "科研服务",
+       imageUrl: "../../img/images/icons/图标.png"
+     } */
+    icons: {
+      type: Array,
+      value: []
+    },
+    /* 接收标题 */
+    title: {
+      type: String,
+      value: ""
+    }
   }
 })

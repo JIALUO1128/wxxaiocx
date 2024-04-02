e59e5a6d7de7c7e4b31b58302ac984e55bd74566
JIALO <jyls1128@gmail.com>
2024-04-02 21:32:50 +0800

    添加my的js

--- pages/My/My.js
+++ pages/My/My.js
@@ -5,6 +5,107 @@ Page({
    * 页面的初始数据
    */
   data: {
+		messagelist: [{
+      id: 1,
+      text1: "补课申请",
+      text2: "任课教师:系统管理员",
+      imageUrl: "../../img/images/icons/yellow-bell.png",
+      time: "昨天"
+    }, {
+      id: 2,
+      text1: "域名申请",
+      text2: "工作部门:系统维护 请假申请人:xxx",
+      imageUrl: "../../img/images/icons/Internet.png",
+      time: "昨天"
+    }, {
+      id: 3,
+      text1: "出国旅游申请",
+      text2: "工作部门:系统维护 请假申请人:xxx",
+      imageUrl: "../../img/images/icons/apply.png",
+      time: "昨天"
+    }, ],
+    icons: [{
+        id: 1,
+        iconTitle: "学校概况",
+        imageUrl: "../../img/images/icons/school.png"
+      },
+      {
+        id: 2,
+        iconTitle: "学校地图",
+        imageUrl: "../../img/images/icons/map.png"
+      },
+      {
+        id: 3,
+        iconTitle: "我的消费",
+        imageUrl: "../../img/images/icons/money.png"
+      },
+      {
+        id: 4,
+        iconTitle: "电子邮箱",
+        imageUrl: "../../img/images/icons/email.png"
+      },
+      {
+        id: 5,
+        iconTitle: "图书借阅",
+        imageUrl: "../../img/images/icons/book.png"
+      },
+      {
+        id: 6,
+        iconTitle: "获奖成果",
+        imageUrl: "../../img/images/icons/trophy.png"
+      },
+      {
+        id: 7,
+        iconTitle: "我的党建",
+        imageUrl: "../../img/images/icons/redflag.png"
+      },
+      {
+        id: 8,
+        iconTitle: "网上审批",
+        imageUrl: "../../img/images/icons/people.png"
+      },
+    ],
+    icons2: [{
+        id: 1,
+        iconTitle: "教务系统",
+        imageUrl: "../../img/images/icons/图标J0Q6Uhy@1x.png"
+      },
+      {
+        id: 2,
+        iconTitle: "学费缴纳",
+        imageUrl: "../../img/images/icons/图标243Iy2wj@1x.png"
+      },
+      {
+        id: 3,
+        iconTitle: "星标课程",
+        imageUrl: "../../img/images/icons/图标3YjyKok1@1x.png"
+      },
+      {
+        id: 4,
+        iconTitle: "学生科研",
+        imageUrl: "../../img/images/icons/图标4F5d1t6g@1x.png"
+      },
+      {
+        id: 5,
+        iconTitle: "在线选课",
+        imageUrl: "../../img/images/icons/图标6PB7S5DH@1x.png"
+      },
+      {
+        id: 6,
+        iconTitle: "专业设置",
+        imageUrl: "../../img/images/icons/图标7r3ljxQh@1x.png"
+      },
+      {
+        id: 7,
+        iconTitle: "学霸名单",
+        imageUrl: "../../img/images/icons/图标8Q0MuC0z@1x.png"
+      },
+      {
+        id: 8,
+        iconTitle: "学生信息",
+        imageUrl: "../../img/images/icons/图标9Y998f10@1x.png"
+      },
+    ],
 
   },
 
